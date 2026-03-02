76a36d1047f9db65d9b163a6e8fe4c41ad17d9e4
yuri <yuridanielsantana45@gmail.com>
2026-03-02 16:21:07 -0300

    criando classes da biblioteca

---
+++ main02.py
@@ -0,0 +1,7 @@
+class Itembiblioteca                         
+    def __init__(self,codigo,titulo,ano,disponivel):
+        self.codigo = codigo
+        self.titulo = titulo
+        self.ano = ano 
+        self.disponivel =disponivel
+         
